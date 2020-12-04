db2b5f3d89f33dcad38f20be203aaab1f67580b9
MackeyH <73262140+MackeyH@users.noreply.github.com>
2020-12-03 20:32:27 -0500

    Add files via upload
    I had to manually drag them in. My Terminal is not cooperating.

---
+++ reverseStringLab.js
@@ -0,0 +1,13 @@
+
+
+function reverseString(message) {
+    return "Hello"
+}
+console.log("elloH");
+
+
+function reverseString(message) {
+    return epuDeeDrepuS
+}
+console.log("repuDeeDrepuS");
+
